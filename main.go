09902bfff793503998f9cef8276499f7edb92048
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-08-20 19:28:52 +0530

    taking user input

--- main.go
+++ main.go
@@ -10,16 +10,27 @@ func main(){
 	// checking the data types of the variables
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
-
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v tickets are left", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	// Ask user for their name
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
+	
+	// Ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName) //pointer
 
-	userName = "Simon"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets", userName, userTickets)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter no. of tickets:")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets, You will receive confirmation mail at %v\n", firstName, lastName, userTickets,email)
 }
