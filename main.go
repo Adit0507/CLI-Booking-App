91d58098438b51bfd6acb50d887e2003500f5360
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-08-22 11:45:14 +0530

    arrays and slices

--- main.go
+++ main.go
@@ -13,28 +13,32 @@ func main(){
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v tickets are left", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-
+	
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
+	bookings:= []string{} 
 	
 	// Ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName) //pointer
-
+	
 	fmt.Println("Enter your last name: ")
 	fmt.Scan(&lastName)
 	
 	fmt.Println("Enter your email address")
 	fmt.Scan(&email)
-
+	
 	fmt.Println("Enter no. of tickets:")
+	
 	fmt.Scan(&userTickets)
-
+	
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " "+ lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets, You will receive confirmation mail at %v\n", firstName, lastName, userTickets,email)
-
 	fmt.Printf("%v remaining Tickets for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
