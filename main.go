ac37195928c21ef2751292af4a72b09a1d6f027b
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-09-01 10:00:56 +0530

    structs

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strconv"
 )
 
 // := only applies to variables
 var conferenceName ="Go Conference"
 const conferenceTickets uint = 50
 var remainingTickets uint = 50
-var bookings = make([]map[string]string, 0) 
+var bookings = make([]UserData, 0) 
+ 
+type UserData struct {
+	firstName string
+	lastName string
+	email string
+	numberOfTickets uint
+	isOptedInForNewsLetter bool
+}
 
 func main(){
 	
@@ -60,7 +67,7 @@ func greetUsers(){
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	
 	return firstNames
@@ -91,13 +98,12 @@ func getUserInput() (string, string, string, uint){
 func bookTicket(userTickets uint,firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
 
-	// create map for a user
-	var userData = make(map[string] string)
-	userData["firstName"]= firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-
-	userData["number"]= strconv.FormatUint(uint64(userTickets), 10)
+	var userData = UserData {
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		numberOfTickets: userTickets,
+	}
 
 	bookings = append(bookings, userData)
 	fmt.Printf("List of bookings is %v\n", bookings)
